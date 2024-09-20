103427202e92b90eb7ca51095ed9eae5f7a9683c
sahil-byte448 <202201021.sahilgps@student.xavier.ac.in>
2024-09-20 17:03:32 +0530

    ghfhg

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -50,7 +50,7 @@ const Home: React.FC = () => {
         <div className='border-2 py-4 px-16 mt-8 rounded-lg text-4xl w-fit'>Airdrop Machine</div>
   
         <div className='flex flex-col border-2 p-8 mt-8 rounded-lg w-fit gap-3'>
-          Enter Your Address To Calim :
+          Enter Your tester To Calim :
           <input placeholder='Enter your wallet address' type="text" className='border p-2 rounded-lg w-56 text-black' onChange={(e) => setWalletAddress(e.target.value)} />
           <button className='border-2 p-2 rounded-lg bg-blue-500 hover:bg-blue-400 text-white disabled:bg-blue-400' onClick={handleClaim} disabled={loading}>{loading ? "Please wait.. " : "Claim"}</button>
         </div>
